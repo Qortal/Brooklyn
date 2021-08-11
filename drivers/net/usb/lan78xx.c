f01da3e636570eb80517ce89faba710d1211cd36
Scare  Crowe <84860158+CWDSYSTEMS@users.noreply.github.com>
2021-08-12 00:35:13 +0500

    Massive update....
    * Is there a moose on the loose?
    * Yes! There is a moose on the loose !
    * His name is Seany

--- drivers/net/usb/lan78xx.c
+++ drivers/net/usb/lan78xx.c
@@ -1168,7 +1168,7 @@ static int lan78xx_link_reset(struct lan78xx_net *dev)
 {
 	struct phy_device *phydev = dev->net->phydev;
 	struct ethtool_link_ksettings ecmd;
-	int ladv, radv, ret;
+	int ladv, radv, ret, link;
 	u32 buf;
 
 	/* clear LAN78xx interrupt status */
@@ -1179,9 +1179,12 @@ static int lan78xx_link_reset(struct lan78xx_net *dev)
 	/* Acknowledge any pending PHY interrupt, lest it be the last */
 	phy_read(phydev, LAN88XX_INT_STS);
 
+	mutex_lock(&phydev->lock);
 	phy_read_status(phydev);
+	link = phydev->link;
+	mutex_unlock(&phydev->lock);
 
-	if (!phydev->link && dev->link_on) {
+	if (!link && dev->link_on) {
 		dev->link_on = false;
 
 		/* reset MAC */
@@ -1194,7 +1197,7 @@ static int lan78xx_link_reset(struct lan78xx_net *dev)
 			return -EIO;
 
 		del_timer(&dev->stat_monitor);
-	} else if (phydev->link && !dev->link_on) {
+	} else if (link && !dev->link_on) {
 		dev->link_on = true;
 
 		phy_ethtool_ksettings_get(phydev, &ecmd);
@@ -1483,9 +1486,14 @@ static int lan78xx_set_eee(struct net_device *net, struct ethtool_eee *edata)
 
 static u32 lan78xx_get_link(struct net_device *net)
 {
-	phy_read_status(net->phydev);
+	u32 link;
 
-	return net->phydev->link;
+	mutex_lock(&net->phydev->lock);
+	phy_read_status(net->phydev);
+	link = net->phydev->link;
+	mutex_unlock(&net->phydev->lock);
+
+	return link;
 }
 
 static void lan78xx_get_drvinfo(struct net_device *net,
