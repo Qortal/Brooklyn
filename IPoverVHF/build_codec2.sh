db0c4cb4dc407e7e8c134ed566ccf80546593523
Scare  Crowe <84860158+CWDSYSTEMS@users.noreply.github.com>
2021-11-11 19:07:47 +0500

    Remove dsat from mainstream

--- IPoverVHF/build_codec2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-# automate local build of codec2
-git clone https://github.com/drowe67/codec2.git
-cd codec2 && mkdir -p build_linux && cd build_linux
-cmake ../ && make
--- IPoverVHF/build_csdr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-# Automate building of csdr library, which is used by rtl_fsk application
-PIRIP_DIR=$(pwd)
-git clone https://github.com/ha7ilm/csdr.git
-cd csdr && make
