9c4c3c24a5a3d20a522fca5485074811e470afe2
Raziel K. Crowe <101006007+RazielCrowe@users.noreply.github.com>
2022-09-09 14:10:02 +0500

    redo

---
+++ include/soc/bcm2835/raspberrypi-clocks.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __SOC_RASPBERRY_CLOCKS_H__
+#define __SOC_RASPBERRY_CLOCKS_H__
+
+#if IS_ENABLED(CONFIG_CLK_RASPBERRYPI)
+unsigned long rpi_firmware_clk_get_max_rate(struct clk *clk);
+unsigned long rpi_firmware_clk_get_min_rate(struct clk *clk);
+#else
+static inline unsigned long rpi_firmware_clk_get_max_rate(struct clk *clk)
+{
+	return ULONG_MAX;
+}
+
+static inline unsigned long rpi_firmware_clk_get_min_rate(struct clk *clk)
+{
+	return 0;
+}
+#endif
+
+#endif /* __SOC_RASPBERRY_CLOCKS_H__ */
